4398ae5ad7e676f1cee10e7b17cd9634ff304666
Alejandro Rodriguez Castro <alejandrogrc@gmail.com>
2021-06-07 01:13:30 -0300

    Enzyme test

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
-export const GifGridItem = ( {id, title,url} ) => {
-    console.log(id,title,url)
+export const GifGridItem = ( {title,url} ) => {
     return (
         <div className='card animate__animated animate__fadeInDown'>
             <img src={ url } alt={ title } />
